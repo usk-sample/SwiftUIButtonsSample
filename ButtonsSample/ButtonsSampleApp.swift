768b690bc2508ecee0884dc1b98387511180a5dd
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-07 13:48:28 +0900

    Initial Commit

---
+++ ButtonsSample/ButtonsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsSampleApp.swift
+//  ButtonsSample
+//
+//  Created by Yusuke Hasegawa on 2021/06/07.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
